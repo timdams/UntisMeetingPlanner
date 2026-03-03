ca1288a4b450c3cf57c5d871430ef2422b80e2f4
timdams <dams.tim@telenet.be>
2026-03-03 14:28:03 +0100

    bug

--- MEETINGPLANNER2026/vite.config.ts
+++ MEETINGPLANNER2026/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MeetingPlanner2026/', // Voeg dit toe
+  base: '/UntisMeetingPlanner/', // Zorg dat dit overeenkomt met je repository naam
   server: {
     port: 3000,
   },
